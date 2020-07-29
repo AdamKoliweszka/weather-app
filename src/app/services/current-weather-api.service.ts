ebd1a40e5d3c887acbf8cd583c9fc3ea2c0e238a
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-07-29 19:50:38 +0200

    add api service

---
+++ src/app/services/current-weather-api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: "root",
+})
+export class CurrentWeatherApiService {
+  constructor(private httpClient: HttpClient) {}
+
+  getActualWeather(): Observable<any> {
+    return this.httpClient.get<any>(
+      environment.weatherUrl +
+        "?id=" +
+        environment.cityId +
+        "&appid=" +
+        environment.secretApiKey
+    );
+  }
+}
