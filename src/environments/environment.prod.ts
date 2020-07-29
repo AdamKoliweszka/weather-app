30ccf0a62fab3bb40c5ccd5b5cea1b7ace8dc596
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-07-29 19:12:21 +0200

    add env variables for api service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  cityId: 3081368, //Id of Wrocław
+  secretApiKey: "2187afa1c754181060c0fe55a2d619c4",
+  weatherUrl: "api.openweathermap.org/data/2.5/weather",
 };
