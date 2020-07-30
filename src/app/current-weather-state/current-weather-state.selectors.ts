b7787bd00cf04e63154b40393ededb30ed4ac1da
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-07-30 15:36:15 +0200

    add store service to component

--- src/app/current-weather-state/current-weather-state.selectors.ts
+++ src/app/current-weather-state/current-weather-state.selectors.ts
@@ -8,30 +8,30 @@ export const selectCurrentWeatherState = createFeatureSelector<
 
 export const selectTemperature = createSelector(
   selectCurrentWeatherState,
-  (state) => state.main.temp
+  (state) => (state.main ? state.main.temp : null)
 );
 
 export const selectFeelsTemperature = createSelector(
   selectCurrentWeatherState,
-  (state) => state.main.feels_like
+  (state) => (state.main ? state.main.feels_like : null)
 );
 
 export const selectMinTemperature = createSelector(
   selectCurrentWeatherState,
-  (state) => state.main.temp_min
+  (state) => (state.main ? state.main.temp_min : null)
 );
 
 export const selectMaxTemperature = createSelector(
   selectCurrentWeatherState,
-  (state) => state.main.temp_max
+  (state) => (state.main ? state.main.temp_max : null)
 );
 
 export const selectPressure = createSelector(
   selectCurrentWeatherState,
-  (state) => state.main.pressure
+  (state) => (state.main ? state.main.pressure : null)
 );
 
 export const selectHumidity = createSelector(
   selectCurrentWeatherState,
-  (state) => state.main.humidity
+  (state) => (state.main ? state.main.humidity : null)
 );
