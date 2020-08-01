7fa1b7146599bf4d7760e4fce78b831294a0a0e4
AdamKoliweszka <adamkoliweszka@gmail.com>
2020-08-01 22:14:19 +0200

    add animation to refresh button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,15 @@ import { environment } from "src/environments/environment";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { CurrentWeatherStateModule } from "./current-weather-state/current-weather-state.module";
 import { CurrentWeatherWidgedComponent } from "./current-weather-widged/current-weather-widged.component";
-import { WidgedRefreshButtonComponent } from './widged-refresh-button/widged-refresh-button.component';
-import { WidgedTemperatureComponent } from './widged-temperature/widged-temperature.component';
-import { TemperatureToCelsiusPipe } from './temperature-to-celsius.pipe';
-import { WidgedCalculationInfoComponent } from './widged-calculation-info/widged-calculation-info.component';
-import { WeatherStatusComponent } from './weather-status/weather-status.component';
-import { HumiditiLevelComponent } from './humiditi-level/humiditi-level.component';
-import { CloudinessComponent } from './cloudiness/cloudiness.component';
-import { AthmosphericPressureComponent } from './athmospheric-pressure/athmospheric-pressure.component';
+import { WidgedRefreshButtonComponent } from "./widged-refresh-button/widged-refresh-button.component";
+import { WidgedTemperatureComponent } from "./widged-temperature/widged-temperature.component";
+import { TemperatureToCelsiusPipe } from "./temperature-to-celsius.pipe";
+import { WidgedCalculationInfoComponent } from "./widged-calculation-info/widged-calculation-info.component";
+import { WeatherStatusComponent } from "./weather-status/weather-status.component";
+import { HumiditiLevelComponent } from "./humiditi-level/humiditi-level.component";
+import { CloudinessComponent } from "./cloudiness/cloudiness.component";
+import { AthmosphericPressureComponent } from "./athmospheric-pressure/athmospheric-pressure.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -39,6 +40,7 @@ import { AthmosphericPressureComponent } from './athmospheric-pressure/athmosphe
     AppRoutingModule,
     HttpClientModule,
     CurrentWeatherStateModule,
+    BrowserAnimationsModule,
     StoreModule.forRoot({}),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
--- src/app/widged-refresh-button/widged-refresh-button.component.ts
+++ src/app/widged-refresh-button/widged-refresh-button.component.ts
@@ -1,9 +1,34 @@
 import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from "@angular/animations";
 
 @Component({
   selector: "app-widged-refresh-button",
   templateUrl: "./widged-refresh-button.component.html",
   styleUrls: ["./widged-refresh-button.component.scss"],
+  animations: [
+    trigger("rotateOnRefreshing", [
+      state(
+        "1",
+        style({
+          transform: "rotate(0)",
+        })
+      ),
+      state(
+        "0",
+        style({
+          transform: "rotate(180deg)",
+        })
+      ),
+      transition("0=>1", animate("1500ms")),
+      transition("1=>0", animate("1500ms")),
+    ]),
+  ],
 })
 export class WidgedRefreshButtonComponent implements OnInit {
   @Input() isInRefreshingState: boolean;
@@ -14,6 +39,7 @@ export class WidgedRefreshButtonComponent implements OnInit {
   ngOnInit() {}
 
   refreshWeather() {
+    // this.currentState = this.currentState === "initial" ? "final" : "initial";
     if (!this.isInRefreshingState) this.clickButton.emit();
   }
 }
